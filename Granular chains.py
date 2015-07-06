eef61604f5881197f0544578830da269a5347408
Juan Lopez <jc.lopez11@uniandes.edu.co>
2015-07-06 11:55:07 -0400

    The code works properly

--- Granular chains.py
+++ Granular chains.py
@@ -1,14 +1,14 @@
 
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[4]:
+# In[2]:
 
 def step_size(M, qp, n):
     """
@@ -26,7 +26,7 @@ def step_size(M, qp, n):
     return DP
 
 
-# In[79]:
+# In[3]:
 
 #LZB Multiple impact models
 def integration(qp, M, W, E, K, n, es, Dp):
@@ -46,21 +46,21 @@ def integration(qp, M, W, E, K, n, es, Dp):
     EE = [E]
     #reltive speed
     dp = np.zeros(qp.shape[0]-1)
-    #relative speed matrux
+    #relative speed matrix
     ff = [dp]
     #normal impulse
     P = np.zeros(qp.shape[0]-1)
     #normal impulse matrix
     PP = [P]
     #contact force
-    #lamb = np.zeros(qp.shape[0]-1)
+    lamb = np.zeros(qp.shape[0]-1)
     #contact force matrix
-    #lambb = [lamb]
+    lambb = [lamb]
     
     #initial values
-    #for j in xrange(qp.shape[0]-1):
-    #    dp[j] = qp[j] - qp[j+1]
-    #    lamb[j] = (1 + n[j]) ** (n[j]/(n[j] + 1)) * K[j] ** (1/(n[j] + 1)) * E[j] ** (n[j]/(n[j] + 1))
+    for j in xrange(qp.shape[0]-1):
+        dp[j] = qp[j] - qp[j+1]
+        lamb[j] = (1 + n[j]) ** (n[j]/(n[j] + 1)) * K[j] ** (1/(n[j] + 1)) * E[j] ** (n[j]/(n[j] + 1))
         
     #Integration
     
@@ -69,16 +69,16 @@ def integration(qp, M, W, E, K, n, es, Dp):
     #vector that stores the impulse space
     pp = [p]
     #time
-    #t = 0
+    t = 0
     #vector that stores the time space
-    #tt = [t]
+    tt = [t]
     Termination = False
     #termination = true: impact is over
     #termination = false: otherwise
     k = 0
     
     #Compute the distributing vector Ta
-    Ta = np.zeros(qp.shape[0])
+    Ta = np.zeros(qp.shape[0] - 1)
     ni = np.zeros(qp.shape[0] - 1)
     Ki = np.zeros(qp.shape[0] - 1)
     dpi = np.zeros(qp.shape[0] - 1)
@@ -95,11 +95,15 @@ def integration(qp, M, W, E, K, n, es, Dp):
     #for speed purposes
     
     rang = range(qp.shape[0] - 1)
-    while (not Termination) | k < 100000:
+    m = np.zeros(M.shape)
+    for j in xrange(M.shape[0]):
+        m[j][j] = 1 / M[j][j]
+    #print K
+    while Termination == False:
         
         Termination = True
         for j in rang:
-            if E[j] == 0:
+            if E[j] <= 0.:
                 if dp[j] <= 0:
                     flag[j] = 0
                 else:
@@ -109,6 +113,13 @@ def integration(qp, M, W, E, K, n, es, Dp):
                 flag[j] = 2
                 Termination = False
         
+        if k == 20000:
+            Termination = True
+        
+        if Termination:
+            print 'termination'
+            print dp
+        
         #Condition of contact
         PrimaryContactInVel = True
         #True: the primary contact is selected according to the relative velocity
@@ -116,7 +127,7 @@ def integration(qp, M, W, E, K, n, es, Dp):
         
         i = 0
         maxE = max(E)
-        if maxE == 0:
+        if maxE <= 0:
             PrimaryContactInVel = True
             for j in rang:
                 if dp[i] < dp[j]:
@@ -140,22 +151,27 @@ def integration(qp, M, W, E, K, n, es, Dp):
                 elif flag[j] == 1:
                     Ai[j] = (dp[j] * Dp) ** (n[j] / (n[j] + 1)) / (E[i] ** (n[i] / (n[i] + 1)))
                     Ta[j] = (ni[j] * Ki[j] * Ai[j]) ** (n[j] + 1)
-                    dP[j] = Dp*Ta[j]
                 elif flag[j] ==2:
                     Ei[j] = (E[j] ** (n[j] / (n[j] + 1))) / (E[i] ** (n[i] / (n[i] + 1)))
                     Ta[j] = ni[j] * Ki[j] * Ei[j]
-                    dP[j] = Dp*Ta[j]
-        
-        qp += Dp * np.dot(np.dot(np.linalg.inv(M), W), Ta)
+            dP[j] = Dp * Ta[j]
+            
+        qp += Dp * np.dot(np.dot(m, W), Ta)
         for j in rang:
             tmp = dp[j]
             dp[j] = qp[j] - qp[j+1]
             P[j] += dP[j]
             if dp[j] >= 0:
-                E[j] += ((tmp + dp[j]) / 2) * dP[j]
+                if E[j] + ((tmp + dp[j]) / 2) * dP[j] < 0:
+                    E[j] = 0
+                else:
+                    E[j] += ((tmp + dp[j]) / 2) * dP[j]
             else:
-                E[j] += (1 / (es[j] ** 2)) *  ((tmp + dp[j]) / 2) * dP[j]
-            #lamb[j] += ((1 + n[j]) ** (n[j] / (n[j] + 1))) * (K[j] ** (1 / (n[j] + 1))) * (E[j] ** (n[j] / (n[j] + 1)))
+                if E[j] + ((tmp + dp[j]) / 2) * dP[j] < 0:
+                    E[j] = 0
+                else:
+                    E[j] += (1 / (es[j] ** 2)) *  ((tmp + dp[j]) / 2) * dP[j]
+            lamb[j] = ((1 + n[j]) ** (n[j] / (n[j] + 1))) * (K[j] ** (1 / (n[j] + 1))) * (E[j] ** (n[j] / (n[j] + 1)))
         
         #Appends variables in memory
         EE = np.concatenate((EE, [E]), axis=0)
@@ -164,66 +180,72 @@ def integration(qp, M, W, E, K, n, es, Dp):
         pp = np.concatenate((pp, [p]), axis=0)
         qq = np.concatenate((qq, [qp]), axis=0)
         ff = np.concatenate((ff, [dp]), axis=0)
-        #lambb = np.concatenate((lambb, [lamb]), axis=0)
+        lambb = np.concatenate((lambb, [lamb]), axis=0)
+        t += Dp/lamb[i]
+        tt = np.concatenate((tt, [t]), axis=0)
         #Advance to next step
         k += 1
         #print qq.shape
-        if k%1000 == 0:
-            print k / 1000
-        for j in rang:
-            if E[j] < 0:
-                print E
-    return {'relative':ff, 'impulse_space':pp, 'momentum':PP, 'Potential':EE, 'velocity':qq}
+        #if k%1000 == 0:
+        #    print k / 1000
+        #print k
+        #print Ta
+        #for j in rang:
+        #    if E[j] < 0:
+        #        print 'the error %g' % (E[j])
+        #print flag
+        #print i
+        #print lamb
+        #print Termination
+    return {'relative':ff, 'impulse_space':pp, 'normal_impulse':PP, 'Potential':EE, 'velocity':qq, 'time':tt, 'force':lambb}
 
 
-# In[82]:
+# In[4]:
 
 #Book example
 qp1 = np.array([1.,0.,0.,0.,0.,0.,0.,0.,0.,0.])
 M1 = np.multiply(0.0183311931337,np.eye(10))
-W1 = np.array([[-1.,0.,0.,0.,0.,0.,0.,0.,0.,0.], [1.,-1.,0.,0.,0.,0.,0.,0.,0.,0.], [0.,1.,-1.,0.,0.,0.,0.,0.,0.,0.], [0.,0.,1.,-1.,0.,0.,0.,0.,0.,0.], [0.,0.,0.,1.,-1.,0.,0.,0.,0.,0.], [0.,0.,0.,0.,1.,-1.,0.,0.,0.,0.], [0.,0.,0.,0.,0.,1.,-1.,0.,0.,0.], [0.,0.,0.,0.,0.,0.,1.,-1.,0.,0.], [0.,0.,0.,0.,0.,0.,0.,1.,-1.,0.], [0.,0.,0.,0.,0.,0.,0.,0.,1.,-1.]])
+W1 = np.array([[-1.,0.,0.,0.,0.,0.,0.,0.,0.], [1.,-1.,0.,0.,0.,0.,0.,0.,0.,], [0.,1.,-1.,0.,0.,0.,0.,0.,0.], [0.,0.,1.,-1.,0.,0.,0.,0.,0.], [0.,0.,0.,1.,-1.,0.,0.,0.,0.], [0.,0.,0.,0.,1.,-1.,0.,0.,0.], [0.,0.,0.,0.,0.,1.,-1.,0.,0.], [0.,0.,0.,0.,0.,0.,1.,-1.,0.], [0.,0.,0.,0.,0.,0.,0.,1.,-1.], [0.,0.,0.,0.,0.,0.,0.,0.,1.]])
 K1 = np.multiply(10515947.0023, np.ones(9))
-E1 = np.multiply(1e-10, np.ones(9))
+#E1 = np.multiply(1e-10, np.ones(9))
+E1 = np.zeros(9)
 n1 = np.multiply(1.5, np.ones(9))
 es1 = np.ones(9)
-Dp1 = 1e-3
-#g = integration(qp, M, W, K, E, n, es, Dp)
+Dp1 = 1e-5
+g = integration(qp1, M1, W1, E1, K1, n1, es1, Dp1)
 #print g['velocity']
 
 
-# In[ ]:
-
-#Generic example
-qp0 = np.array([1.,0.,0.,0.,0.,0.,0.])
-M0 = np.eye(7)
-W0 = np.array([[-1.,0.,0.,0.,0.,0.,0.], [1.,-1.,0.,0.,0.,0.,0.], [0.,1.,-1.,0.,0.,0.,0.], [0.,0.,1.,-1.,0.,0.,0.], [0.,0.,0.,1.,-1.,0.,0.], [0.,0.,0.,0.,1.,-1.,0.], [0.,0.,0.,0.,0.,1.,-1.]])
-K0 = np.ones(7)
-E0 = np.array([1e-10,1e-10,1e-10,1e-10,1e-10,1e-10])
-n0 = np.array([1.5,1.5,1.5,1.5,1.5,1.5])
-es0 = np.array([1.,1.,1.,1.,1.,1.])
-Dp0 = 1e-3
-g0 = integration(qp0, M0, W0, K0, E0, n0, es0, Dp0)
-#print g['velocity']
-
-
-# In[ ]:
+# In[5]:
 
 #g = integration(qp1, M1, W1, K1, E1, n1, es1, Dp1)
-plt.plot(g0['impulse_space'], g0['velocity'])
+plt.plot(g['time'], g['velocity'])
 plt.show()
-print g0['velocity'].shape
+print g['time']
 
 
-# In[68]:
+# In[6]:
 
 c = 203e6 / (0.91 * 2.)
 v = (0.01 ** 3.) * (math.pi * 3. * 7780.) / 4.
-o = ((0.005 ** 0.5) * c * 4.) / (3. * 0.91)
+o = ((0.005 ** 0.5) * c * 4.) / 3.
 print v
 print o
 #print K
 #print 11555985.7168 * np.ones(9)
 print K1
+
+
+# In[7]:
+
+af = step_size(M1, qp1, 100000.)
+print af
+
+
+# In[8]:
+
+M = [[0.5,0.,0.],[0.,0.4,0.],[0.,0.,0.1]]
+print np.linalg.inv(M)
 
 
 # In[ ]:
